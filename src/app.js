6c4e1691f3a3fd9a85fe9f2f3fcd56400f3e29e5
xcessive <xcessive@gmail.com>
2021-11-20 16:33:15 +0700

    Add Linter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import "regenerator-runtime";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/style.css";
+import "./style/style.scss";
 
 import main from "./script/view/main.js";
 
